b4ab76c2ff817ba0f31cad598fb1332dd80b2cf7
ymaren <ricco@freemail.ru>
2016-04-10 23:27:48 +0000

    add new file

--- js/javascript.js
+++ js/javascript.js
@@ -178,21 +178,6 @@ $('#sentform').click(function() {
 });
 
 
-$('#btn-send').click(function() {
-  // Please set your email in url field
-  $.ajax({
-      url: "https://formspree.io/evgmarenkov@gmail.com",  
-      method: "POST",
-      data: {
-             email: email.value,
-             message: msg.value
-             },
-      dataType: "json"
-  }).done(function() {
-     $('#container').html('<h1>Thank you!</h1>')
-  } );
-});
-
 
 
 
